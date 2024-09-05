ac3f7e3946a5e34d0f47631e3c3ba94da2de9ed8
samettopcu88 <samet.topcu.88@icloud.com>
2024-09-06 01:39:27 +0300

    güncelleme.

--- VideoSubtitleApp/Models/Video.cs
+++ VideoSubtitleApp/Models/Video.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string FilePath { get; set; } // Videonun yüklendiği dosya yolu
+        public string FilePath { get; set; }
         public DateTime UploadDate { get; set; }
         public Subtitle Subtitle { get; set; }
     }
